85af58f4f0242b3624c66ce2f709ac3225d3a1bd
Hideaki Kawai <kayhide@gmail.com>
2020-04-27 11:14:16 +0900

    Update Client tests

--- spec/shutwork/client_spec.rb
+++ spec/shutwork/client_spec.rb
@@ -6,19 +6,34 @@ RSpec.describe Shutwork::Client do
   let(:token) { Shutwork::Token.read }
 
   describe "#me" do
-    it "returns me" do
+    it "returns account" do
       VCR.use_cassette "client/me" do
         res = subject.me
-        expect(res).not_to be nil
+        item = JSON.parse(res)
+        expect(item).to include(*%w(account_id name))
       end
     end
   end
 
   describe "#rooms" do
-    it "returns rooms" do
+    it "returns list of rooms" do
       VCR.use_cassette "client/rooms" do
         res = subject.rooms
-        expect(res).not_to be nil
+        items = JSON.parse(res)
+        expect(items.length).to eq 3
+        expect(items).to all include(*%w(room_id name))
+      end
+    end
+  end
+
+  describe "#room_messages" do
+    it "returns list of messages" do
+      VCR.use_cassette "client/room_messages" do
+        res = subject.room_messages 1
+        items = JSON.parse(res)
+        expect(items.length).to eq 4
+        expect(items).to all include(*%w(message_id body))
+        expect(items).to all include("account" => include(*%w(name)))
       end
     end
   end
